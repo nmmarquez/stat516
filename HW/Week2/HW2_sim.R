ab44c470d164559875fd0beda39048205a24728b
Neal Marquez <nmmarquez@gmail.com>
2017-10-12 00:11:11 -0700

    update HW2

---
+++ HW/Week2/HW2_sim.R
@@ -0,0 +1,21 @@
+rm(list=ls())
+
+N <- 100000
+p <- 1
+
+# 2 independent binomial variables with p = .5
+Y <- rbinom(N, 1, .5)
+Z <- rbinom(N, 1, .5)
+
+# make X a function of Y and Z susch that if Y == Z take 1 probability (p) and
+# if Y != Z take 1 - p
+X <- rbinom(N, 1, p**(1-((Y + Z) %% 2)) * (1-p)**((Y + Z) %% 2))
+
+RVs <- data.frame(Y, Z, X)
+head(RVs)
+
+summary(RVs)
+summary(subset(RVs, Y == 1))
+summary(subset(RVs, Z == 1))
+summary(subset(RVs, Z == Y))
+summary(subset(RVs, Z != Y))
