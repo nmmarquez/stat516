9ed06a669b929c89fdb223542c3ae4691bac7cbe
Neal Marquez <nmmarquez@gmail.com>
2017-11-15 11:21:38 -0800

    start hw5

---
+++ HW/Week5/HW5.r
@@ -0,0 +1,49 @@
+rm(list=ls())
+
+likfunc <- function(x){
+    b <- exp(x)
+    (.9 - (-exp(-10*b) + exp(-.1*b)))**2
+}
+
+test <- optim(0, likfunc, method="Brent", lower=-20, upper=20)
+b <- exp(test$par)
+pgamma(10, shape=1, rate=b) - pgamma(.1, shape=1, rate=b)
+
+post <- rgamma(10000, 4+1, b+.25)
+hist(post)
+summary(post)
+quantile(post, c(.025, .975))
+
+y <- scan("snoqualmie.txt")
+nodays <- rep(c(365,365,365,366),9) # account for leap years
+june <- matrix(0, nrow=36, ncol=30) # build empty data mtarix
+daysum <- 31*3 + 30 + 29 # our start point is this number of days in the future
+for (i in 1:36){
+    if (i>1) daysum <- daysum + nodays[i-1] # if past the first year add 365|366
+    june[i,] <- (y[daysum+1:30] > 1) * 1 # turn into indicators
+}
+
+n00 <- sum(june[,1:29] == 0 & june[,2:30] == 0)
+n10 <- sum(june[,1:29] == 1 & june[,2:30] == 0)
+n01 <- sum(june[,1:29] == 0 & june[,2:30] == 1)
+n11 <- sum(june[,1:29] == 1 & june[,2:30] == 1)
+
+# sanity check
+sum(n00 + n01 + n10 + n11) == 36*29
+
+p_hat_00 <- n00 / (n00 + n01)
+p_hat_01 <- n01 / (n00 + n01)
+p_hat_10 <- n10 / (n10 + n11)
+p_hat_11 <- n11 / (n10 + n11)
+
+P_hat <- rbind(c(p_hat_00, p_hat_01), c(p_hat_10, p_hat_11))
+colnames(P_hat) <- c("dry", "wet")
+row.names(P_hat) <- c("dry", "wet")
+
+N_obs <- rbind(c(n00, n01), c(n10, n11))
+colnames(N_obs) <- c("dry", "wet")
+row.names(N_obs) <- c("dry", "wet")
+P_stderr <- sqrt(P_hat * (1 - P_hat) / N_obs)
+P_hat - 1.96 * P_stderr
+P_hat + 1.96 * P_stderr
+P_hat
---
+++ notes/stoch_notes.md
@@ -0,0 +1,20 @@
+# Stochastic Modeling of Scientific Data  
+
+## Finite Dimensional Distributions  
+ - Symmetry  
+   - The order of the stes does not matter  
+ - Marginalization  
+   - The extension of the set does not matter  
+
+# Conditional Independence of Variables  
+ - $X \coprod Y$ iff  
+   - $p_{ij} = p_{i+} p_{j+}$ for all $ij$  
+
+# Reminder on Eigen stuff  
+ - For any matrix $A$  
+   - $Av = \lambda v$  Where $v$ is a vector
+
+#Conditional Independence  
+ - $X \coprod Y | Z$ iff  
+   - $ P(X \in A, Y \in B | Z=z) = P(X \in A | Z=z) P( Y \in B | Z=z) $  
+   - $P(X \in A | Y =y ,  Z=z) = P(X \in A | Z=z)$  
