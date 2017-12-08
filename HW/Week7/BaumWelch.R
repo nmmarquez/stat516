a7961ad42e33a07d303c7a16f19e3e80b8b5e185
Neal Marquez <nmmarquez@gmail.com>
2017-12-08 09:58:22 -0800

    update BW plotting code

--- HW/Week7/BaumWelch.R
+++ HW/Week7/BaumWelch.R
@@ -1,5 +1,7 @@
 rm(list=ls())
 library(HMM)
+library(ggplot2)
+library(dplyr)
 
 simulate_run <- function(n){
     P <- matrix(c(.95, .1, .05, .9), 2, 2)
@@ -40,7 +42,7 @@ backward_algorithm <- function(obs, E, P, v, states){
     return(prob_seq)
 }
 
-baumWelchSelf <- function(obs, niter=2000, states=1:2, emissions=1:6){
+baumWelchSelf <- function(obs, niter=99, states=1:2, emissions=1:6, delta=1e-9){
     # initialize parameters
     nStates <- length(states)
     nEmissions <- length(emissions)
@@ -48,8 +50,11 @@ baumWelchSelf <- function(obs, niter=2000, states=1:2, emissions=1:6){
     diag(P) <- 1/nStates * (nStates:1)*2 
     P <- t(apply(P, 1, function(x) x / sum(x)))
     E <- matrix(1/nEmissions, nrow=nStates, ncol=nEmissions)
+    Enew <- E
+    Pnew <- P
     v <- rep(1/nStates, nStates)
     n <- length(obs)
+    deltad <- rep(0, 0)
     
     for(q in 1:niter){
         a_it <- forward_algorithm(obs, E, P, v)[,2:(n+1)]
@@ -65,21 +70,27 @@ baumWelchSelf <- function(obs, niter=2000, states=1:2, emissions=1:6){
             }
             e_itj[,t,] <- e_itj[,t,] / sum(e_itj[,t,])
         }
-        v <- gamma_it[,1]
-        P <- t(apply(apply(e_itj, c(1,3), sum), 1, function(x) x/ sum(x)))
+        #v <- gamma_it[,1]
+        Pnew <- t(apply(apply(e_itj, c(1,3), sum), 1, function(x) x/ sum(x)))
         for(i in 1:nStates){
             for(k in 1:nEmissions){
-                E[i,k] <- sum((obs==k) * gamma_it[i,]) / sum(gamma_it[i,])
+                Enew[i,k] <- sum((obs==k) * gamma_it[i,]) / sum(gamma_it[i,])
             }
         }
-    print(paste0("Finished update number: ", q))
+    d <- sqrt(sum((P - Pnew)^2)) + sqrt(sum((E - Enew)^2))
+    if(d < delta){
+        break
     }
-    return(list(P=P, E=E, v=v, a=a_it, b=b_it, gamma=gamma_it, epsilon=e_itj))
+    deltad <- c(deltad, d)
+    P <- Pnew
+    E <- Enew
+    }
+    return(list(P=P, E=E, v=v, delta=deltad, gamma=gamma_it))
 }
 
 set.seed(123)
 sim1 <- simulate_run(5000)
-est <- baumWelchSelf(sim1$O, 100)
+est <- baumWelchSelf(sim1$O, 500)
 HMMobj <- initHMM(
     1:2, 1:6, c(.5, .5), 
     matrix(c(.8, 1/3, .2, 2/3), 2, 2), 
@@ -95,19 +106,43 @@ est$v
 
 # read in file to run analysis on
 weather <- c(t(as.matrix(read.table("./set3.dat", sep=""))))
-estDat <- baumWelchSelf(weather, niter=100, emissions = 1:16)
-HMMobj <- initHMM(
-    1:2, 1:16, rep(1/2, 2),
-    matrix(c(.8, 1/3, .2, 2/3), 2, 2), 
-    matrix(c(rep(1/16, 16), rep(1/16, 16)), 2, 16, byrow=T))
-estDatPackage <- baumWelch(HMMobj, weather)
-estDatPackage$hmm$transProbs
+estDat <- baumWelchSelf(weather, niter=500, emissions = 1:16)
+# HMMobj <- initHMM(
+#     1:2, 1:16, rep(1/2, 2),
+#     matrix(c(.8, 1/3, .2, 2/3), 2, 2), 
+#     matrix(c(rep(1/16, 16), rep(1/16, 16)), 2, 16, byrow=T))
+# estDatPackage <- baumWelch(HMMobj, weather)
+# estDatPackage$hmm$transProbs
 estDat$P
-estDatPackage$hmm$emissionProbs
+# estDatPackage$hmm$emissionProbs
 estDat$E
-abs(estDatPackage$hmm$emissionProbs - estDat$E)
-estDat$E
+# abs((estDatPackage$hmm$emissionProbs - estDat$E)/ estDat$E)
 
-estDat$a[,1000:1010]
-estDat$b[,1:10]
-tail(t(estDat$b), n=10)
+plot(log(estDat$delta), type="l")
+plot(estDat$gamma[1,1:(30*24)], type="l")
+
+DFHMM <- data.frame(
+    pHS1=estDat$gamma[1,], 
+    date=seq(ISOdate(1985, 5, 1, 0), by=3600, len=length(weather))) %>%
+    mutate(
+    month=as.integer(as.character(date, format="%m")),
+    year=as.integer(as.character(date, format="%Y")),
+    day=as.integer(as.character(date, format="%d")),
+    hour=as.integer(as.character(date, format="%H")),
+    dayhour=day + hour/24)
+
+plot_calendar <- function(DF, x="dayhour"){
+    ggplot(DF, aes_string(x=x, y="pHS1", color="month")) + geom_line() + 
+        facet_wrap(~month)
+}
+
+DFHMM %>% filter(year==1986) %>% plot_calendar
+
+DFHMM %>% group_by(month,day,hour) %>% summarise(pHS1=mean(pHS1)) %>% 
+    mutate(dayhour=day + hour/24) %>% plot_calendar()
+
+DFHMM %>% group_by(month,day) %>% summarise(pHS1=mean(pHS1)) %>% 
+     plot_calendar(x="day")
+    
+DFHMM %>% group_by(month) %>% summarise(pHS1=mean(pHS1)) %>%
+    ggplot(aes(x=month, y=pHS1)) + geom_line()
